479103f156d39447fda3ce3123f7212af5cf5b0b
AugustLight <125802350+AugustLigh@users.noreply.github.com>
2024-04-23 12:49:06 +0300

    fix bugs

--- AminoLightPy/constants.py
+++ AminoLightPy/constants.py
@@ -16,83 +16,83 @@ device_id = gen_deviceId()
 cache = OrderedDict()
 
 class AminoSession(Session):
-    def __init__(self) -> None:
-        super().__init__()
-        self.headers.update({
-            "NDCDEVICEID": device_id,
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": "Apple iPhone13,1 iOS v16.5 Main/3.19.0"
-        })
+	def __init__(self) -> None:
+		super().__init__()
+		self.headers.update({
+			"NDCDEVICEID": device_id,
+			"Accept-Encoding": "gzip, deflate",
+			"User-Agent": "Apple iPhone13,1 iOS v16.5 Main/3.19.0"
+		})
 
-    def request(self, method, url, *args, **kwargs):
+	def request(self, method, url, *args, **kwargs):
 
-        headers = kwargs.get("headers", {})
-        data = kwargs.get("data", None)
+		headers = kwargs.get("headers", {})
+		data = kwargs.get("data", None)
 
-        if method.lower() == "post":
-            if "json" in kwargs and data is None:
-                data = kwargs.get("json") or {}
-                data["timestamp"] = int(time() * 1000)
+		if method.lower() == "post":
+			if "json" in kwargs and data is None:
+				data = kwargs.get("json") or {}
+				data["timestamp"] = int(time() * 1000)
 
-                data = dumps(data)
+				data = dumps(data)
 
-                headers["Content-Type"] = "application/json"
-                headers["NDC-MSG-SIG"] = signature(data)
+				headers["Content-Type"] = "application/json"
+				headers["NDC-MSG-SIG"] = signature(data)
 
-            elif data is None:
-                headers["Content-Type"] = "application/x-www-form-urlencoded"
+			elif data is None:
+				headers["Content-Type"] = "application/x-www-form-urlencoded"
 
-        kwargs["headers"] = headers
-        if data is not None:
-            kwargs["data"] = data
+		kwargs["headers"] = headers
+		if data is not None:
+			kwargs["data"] = data
 
-        response = super().request(method, url, *args, **kwargs)
+		response = super().request(method, url, *args, **kwargs)
 
-        if response.status_code != 200:
-            CheckException(response.text)
+		if response.status_code != 200:
+			CheckException(response.text)
 
-        return response
+		return response
 
 
 def upload_media(self, file: BinaryIO, fileType: str) -> str:
-    """
-    Upload file to the amino servers.
+	"""
+	Upload file to the amino servers.
 
-    **Parameters**
-        - **file** : File to be uploaded.
+	**Parameters**
+		- **file** : File to be uploaded.
 
-    **Returns**
-        - **Success** : Url of the file uploaded to the server.
+	**Returns**
+		- **Success** : Url of the file uploaded to the server.
 
-        - **Fail** : :meth:`Exceptions <aminofix.lib.util.exceptions>`
-    """
-    data = file.read()
+		- **Fail** : :meth:`Exceptions <aminofix.lib.util.exceptions>`
+	"""
+	data = file.read()
 
-    file_hash  = sha1(data).hexdigest()
-    if file_hash in cache:
-        return cache[file_hash]
+	file_hash  = sha1(data).hexdigest()
+	if file_hash in cache:
+		return cache[file_hash]
 
-    if fileType == "audio":
-        t = "audio/aac"
-    elif fileType == "image":
-        t = "image/jpg"
-    elif fileType == "gif":
-        t = "image/gif"
-    else: raise SpecifyType(fileType)
+	if fileType == "audio":
+		t = "audio/aac"
+	elif fileType == "image":
+		t = "image/jpg"
+	elif fileType == "gif":
+		t = "image/gif"
+	else: raise SpecifyType(fileType)
 
-    custom_headers = self.session.headers
-    custom_headers["Content-Type"] = t
+	custom_headers = self.session.headers
+	custom_headers["Content-Type"] = t
 
-    response = self.session.post(
-        url=f"{api}/g/s/media/upload",
-        data=data,
-        headers=custom_headers,
-        stream=True
-    )
+	response = self.session.post(
+		url=f"{api}/g/s/media/upload",
+		data=data,
+		headers=custom_headers,
+		stream=True
+	)
 
-    cache[file_hash] = response.json()["mediaValue"]
-    if len(cache) >= 32:
-        cache.popitem(last=False)
+	cache[file_hash] = response.json()["mediaValue"]
+	if len(cache) >= 32:
+		cache.popitem(last=False)
 
-    return cache[file_hash]
-    
+	return cache[file_hash]
+	
